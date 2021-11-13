101faf5425b727136bc6f3a2cdf7865094aeb6c8
Dante Allievi <danteallievi@gmail.com>
2021-11-13 23:51:34 +0100

    Tailwind added with scripts and config file

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        primary: ["Oswald, sans-serif"],
+        secondary: ["Roboto"],
+      },
+      colors: {
+        primary: "#7E00C6",
+        violet: {
+          100: "#A200FF",
+          200: "#7E00C6",
+          300: "#5A008E",
+          400: "#360055",
+          500: "#12001C",
+        },
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
