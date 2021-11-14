8371c62ba0694ffd66091a6b45935ac6ca0a7a56
Dante Allievi <danteallievi@gmail.com>
2021-11-14 11:40:43 +0100

    Login form added

---
+++ src/components/Forms/Auth/LoginForm.js
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import useUser from "../../../hooks/useUser";
+
+const LoginForm = () => {
+  const { loginUser } = useUser();
+  const initialValue = {
+    username: "",
+    password: "",
+  };
+  const [inputValue, setInputValue] = useState(initialValue);
+
+  const handleChange = (e) => {
+    setInputValue({
+      ...inputValue,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    loginUser(inputValue);
+  };
+
+  return (
+    <form onSubmit={onSubmit} autoComplete="off" noValidate>
+      <div>
+        <label htmlFor="username">Username</label>
+        <input
+          type="text"
+          id="username"
+          placeholder="Username"
+          value={inputValue.username}
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        <label htmlFor="password">Password</label>
+        <input
+          autoComplete="off"
+          value={inputValue.password}
+          type="password"
+          id="password"
+          onChange={handleChange}
+          placeholder="Password"
+        />
+      </div>
+      <div>
+        <button>Log in</button>
+      </div>
+    </form>
+  );
+};
+
+export default LoginForm;
