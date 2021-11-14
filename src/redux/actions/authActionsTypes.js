fa58162e7d7248dfb67f4011d46f5e683306eabe
Dante Allievi <danteallievi@gmail.com>
2021-11-14 11:12:42 +0100

    Redux boiler plate added

---
+++ src/redux/actions/authActionsTypes.js
@@ -0,0 +1,7 @@
+const authActionTypes = {
+  userIsLogged: "USER_LOGGED",
+  loginUser: "LOAD_USER",
+  logoutUser: "LOGOUT",
+  registerUser: "REGISTER_USER",
+};
+export default authActionTypes;
---
+++ src/redux/reducers/userReducer.js
@@ -0,0 +1,43 @@
+import authActionsTypes from "../actions/authActionsTypes";
+
+const userReducer = (
+  user = {
+    isAuthenticated: false,
+    user: {},
+  },
+  action
+) => {
+  let newUserState;
+
+  switch (action.type) {
+    case authActionsTypes.userIsLogged:
+      newUserState = {
+        isAuthenticated: true,
+        user: action.user,
+      };
+      break;
+
+    case authActionsTypes.loginUser:
+      newUserState = {
+        ...user,
+        isAuthenticated: true,
+        user: action.user,
+      };
+      break;
+
+    case authActionsTypes.logoutUser:
+      newUserState = {
+        isAuthenticated: false,
+        user: {},
+      };
+      break;
+
+    default:
+      newUserState = user;
+      break;
+  }
+
+  return newUserState;
+};
+
+export default userReducer;
