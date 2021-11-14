c320a89cfaa37ef29f578b086795907313de5535
Dante Allievi <danteallievi@gmail.com>
2021-11-14 23:22:45 +0100

    Conditional render removed on login and register pages

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -1,13 +1,7 @@
 import LoginForm from "../../components/Forms/Auth/LoginForm";
-import useUser from "../../hooks/useUser";
-import HomePage from "../HomePage/HomePage";
 
 const LoginPage = () => {
-  const { user } = useUser();
-
-  return user.isAuthenticated ? (
-    <HomePage />
-  ) : (
+  return (
     <>
       <h1 className="text-4xl">Login</h1>
       <LoginForm />
